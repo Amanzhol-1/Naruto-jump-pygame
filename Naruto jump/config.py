937a43d20f748d52bd2eec0f38feb379cdcdb325
Amanzhol <69354535+1ncrease@users.noreply.github.com>
2023-04-14 20:12:38 +0600

    d
    d

---
+++ Naruto jump/config.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+screen_width = 1280
+screen_height = 960
+
+platform_width = 100
+fps = 45
+
+doodle_start_position = [240,350]
+
+horizont = 300
+mouse_enabled = False
+transparent_walls = True
+
+platform_count = 10
+gravitation = 0.4
+jump_speed = 15
+spring_speed = 20
+
+platform_y_padding = 7
